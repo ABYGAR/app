1e3729461635722ca37a99b1e5e26dc8c5204fa7
ABYGAR <abisaigqrcia@gmail.com>
2026-04-07 11:28:39 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,59 +1,21 @@
 <?php
 
-use CodeIgniter\Boot;
-use Config\Paths;
-
-/*
- *---------------------------------------------------------------
- * CHECK PHP VERSION
- *---------------------------------------------------------------
- */
-
-$minPhpVersion = '8.2'; // If you update this, don't forget to update `spark`.
-if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION,
-    );
-
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo $message;
-
-    exit(1);
+$minPHPVersion = '8.0';
+if (phpversion() < $minPHPVersion) {
+    die("Your PHP version must be {$minPHPVersion} or higher. Current version: " . phpversion());
 }
 
-/*
- *---------------------------------------------------------------
- * SET THE CURRENT DIRECTORY
- *---------------------------------------------------------------
- */
-
-// Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Ensure the current directory is pointing to the front controller's directory
-if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
-}
+$pathsPath = realpath(FCPATH . '../app/Config/Paths.php');
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
+chdir(__DIR__);
 
-// LOAD OUR PATHS CONFIG FILE
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
+require $pathsPath;
+$paths = new Config\Paths();
 
-$paths = new Paths();
+require rtrim($paths->systemDirectory, '/ ') . '/bootstrap.php';
 
-// LOAD THE FRAMEWORK BOOTSTRAP FILE
-require $paths->systemDirectory . '/Boot.php';
-
-exit(Boot::bootWeb($paths));
+$app = Config\Services::codeigniter();
+$app->initialize();
+$app->run();
